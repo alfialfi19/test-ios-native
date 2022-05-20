7ba05bc3c7d18d735b77a77894e9f09ae8bc5483
Rizki Alfi Ramdhani <rizki.alfi@mekari.com>
2022-05-20 17:16:43 +0700

    Initial Commit

---
+++ test_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_project
+//
+//  Created by MEKARI on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
